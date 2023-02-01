3b0546d4d60455f167b7883c1d09e26b917ff763
Klimek, Mariusz <klimcio@gmail.com>
2023-02-01 21:39:24 +0100

    Untested usage of appsettings + simplification of structure

---
+++ CatechismScrapper/Entities/TableOfContents.cs
@@ -0,0 +1,14 @@
+﻿namespace CatechismScrapper.Entities
+{
+    internal class TableOfContents
+    {
+        public string Source { get; set; } = default!;
+        public IEnumerable<File> Files { get; set; } = Enumerable.Empty<File>();
+    }
+
+    internal class File
+    {
+        public string Chapter { get; set; } = default!;
+        public string? Footnotes { get; set; }
+    }
+}
--- CatechismScrapper/Program.cs
+++ CatechismScrapper/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace CatechismScrapper
+﻿using CatechismScrapper.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace CatechismScrapper
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var configRoot = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var config = new TableOfContents();
+            configRoot.Bind(config);
+
             Console.WriteLine("Hello, World!");
         }
     }
